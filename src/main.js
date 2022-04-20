4082d3dcfebe62b4e77d16add3c6a788af033dc0
noahe123 <eichlernoahj@gmail.com>
2022-04-19 18:39:53 -0700

    updated main

--- src/main.js
+++ src/main.js
@@ -13,3 +13,12 @@ let borderPadding = borderUISize / 3;
 
 // reserve keyboard variables
 let keyF, keyR, keyLEFT, keyRIGHT;
+
+//points breakdown
+//(5) Add your own (copyright-free) background music to the Play scene (5)
+//(5) Allow the player to control the Rocket after it's fired (5)
+//(10) Display the time remaining (in seconds) on the screen (10)
+//(20) Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+//(20) Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) 
+//(20)Implement mouse control for player movement and mouse click to fire (20)
+//(20) Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (20)
